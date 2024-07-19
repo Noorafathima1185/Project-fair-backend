0dd468ad192857b34e8c6b3c1a9c5221f20cbe12
Noorafathima1185 <noorafathimaek1185@gmail.com>
2024-07-19 21:05:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// import dotenv - to load environment variable.
+require('dotenv').config()
+
+
+// import express
+const express = require('express')
+
+
+// import cors
+const cors = require('cors')
+
+// import router
+const router = require('./routes')
+
+
+// create express server
+// creates an express application. the express() function is a top-level function exported by the express module.
+const pfServer = express()
+
+
+// use of cors - to communicate with the view.
+pfServer.use(cors())
+
+
+// server should use json() - returns a middleware which can parse json format.
+pfServer.use(express.json()) // Returns middleware that only parses json and only looks at requests where the Content
+
+
+// use router
+pfServer.use(router)
+
+
+// set port for the server
+PORT = 4000 || process.env.PORT
+
+
+// listen to the port - to resolve the request
+pfServer.listen(PORT,()=>{
+    console.log(`server running successfully at port number : ${PORT}`);
+})
+
+
+// get request
+/* pfServer.get('/',(req,res)=>{
+    // logic
+    res.send('get request recieved')
+}) */
+
+
+// post request
+/* pfServer.post('/',(req,res)=>{
+    // logic
+    res.send('post request recieved')
+}) */
+
+// put request
+/* pfServer.put('/',(req,res)=>{
+    // logic
+    res.send('put request recieved')
+}) */
---
+++ routes.js
@@ -0,0 +1,13 @@
+// 1) import express
+const express = require('express')
+
+
+// 2) create an object for router class
+const router = new express.Router()
+
+
+// 3) set up path for each request from view
+
+
+// 4) export the router
+module.exports = router
