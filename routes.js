559f0e5245ecadeff13b7fc86b83a8e2ee82cbc8
Noorafathima1185 <noorafathimaek1185@gmail.com>
2024-07-20 12:07:34 +0530

    second commit

--- routes.js
+++ routes.js
@@ -1,5 +1,7 @@
 // 1) import express
 const express = require('express')
+// import registerController file
+const userController = require('./controller/userController')
 
 
 // 2) create an object for router class
@@ -8,7 +10,8 @@ const router = new express.Router()
 
 // 3) set up path for each request from view
 
-//
+// register
+router.post('/register',userController.registerController)
 
 
 // 4) export the router
